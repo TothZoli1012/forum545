652f29ddad385178d6c1311fe92edc5d3740a32f
Zoli <Zoli>
2025-09-19 11:55:31 +0200

    data jsnon adatok elore beolvasva

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
+ $filename = "data.json";
+if(file_exists($filename)){
+    $jsonString = file_get_contents($filename);
+    $topics = json_decode($jsonString);
+}
+else{
+    $topics=[];
+}
 $Szoveg="";
 if(isset ($_POST['topic'])){
    $topics = [];
    array_push($topics,$_POST['topic']);
    $jsonString = json_encode($topics);
    $Szoveg=$jsonString;
-   $filename = "data.json";
-   file_put_contents($filename,$jsonString);
+  file_put_contents($filename,$jsonString);
 }
 ?>
 
